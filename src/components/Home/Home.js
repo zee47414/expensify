49cee12f596b77e0d9f7f9d57539665671d1bcd2
Mohammed Zeeshan Khan <zeeshankhan47414@gmail.com>
2024-09-24 04:39:46 +0530

    added the spent income and total balance

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
-const Home = () =>{
+const Home = () => {
     return (
-        <div className='bg-black h-full w-full py-[50px] px-52 ' >
+        <div className='bg-black flex flex-col gap-20 h-full w-full py-[50px] p-4 lg:px-52 ' >
             <div className="flex flex-col items-start">
                 <p className="text-white text-xl">Hi, There</p>
-                <p className="text-white text-3xl text-teal-200" >Zeeshan Khan</p>
+                <p className="text-white text-3xl text-blue-200" >Zeeshan Khan</p>
+            </div>
+            <div className="flex flex-col gap-10 md:flex-row md:space-x-4">
+                <div className="flex-1 border-2 border-blue-400 rounded-xl p-3 bg-[rgb(38,41,51)] relative">
+                    <p className="text-white font-bold">Total Balance: ₹15,000</p>
+                    <span className="bg-blue-500 w-5 h-5 rounded-full block absolute right-[5px] top-[5px]"></span>
+                </div>
+                <div className="flex-1 border-2 border-blue-400 rounded-xl p-3 bg-[rgb(38,41,51)] relative">
+                    <p className="text-white font-bold">Spent: ₹15,000</p>
+                    <span className="bg-blue-500 w-5 h-5 rounded-full block absolute right-[5px] top-[5px]"></span>
+                </div>
+                <div className="flex-1 border-2 border-blue-400 rounded-xl p-3 bg-[rgb(38,41,51)] relative">
+                    <p className="text-white font-bold">Total Balance: ₹15,000</p>
+                    <span className="bg-blue-500 w-5 h-5 rounded-full block absolute right-[5px] top-[5px]"></span>
+                </div>
             </div>
         </div>
     )
